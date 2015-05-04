15eb8579ba4cb7c617ae6ef291b65dec5e9229a4
Douglas Hill <hello@douglashill.co>
2015-05-05 00:06:44 +0100

    Remove Foundation utilities
    The Foundation files are now in the repository at https://github.com/douglashill/DHFoundation.git
    
    The aim is to have these utilities available in frameworks, but UI-related utilities should not be imported in non-UI code so multiple frameworks are required. For dependency management (such as with Carthage), it is easiest if each framework has its own Xcode project and repository.

--- Foundation/DHExceptionCreation.h
+++
@@ -1,6 +0,0 @@
-// Douglas Hill, April 2015
-// https://github.com/douglashill/DHUtilities
-
-@import Foundation;
-
-#define DH_EXCEPTION(name, format, ...) [[NSException alloc] initWithName:name reason:[NSString stringWithFormat:format, __VA_ARGS__] userInfo:nil]
--- Foundation/DHMap.h
+++
@@ -1,6 +0,0 @@
-// Douglas Hill, April 2015
-// https://github.com/douglashill/DHUtilities
-
-typedef id __nullable (^DHIndexToObjectMap)(NSUInteger idx);
-typedef id __nullable (^DHObjectMap)(id __nonnull object);
-typedef NSArray *__nullable (^DHKeyValueMap)(id __nonnull object);
